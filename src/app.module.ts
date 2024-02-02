548cd84bf87d6958f43cd9cf751b00ccc8d9958f
trieutct <96654274+trieutct@users.noreply.github.com>
2024-02-02 11:49:35 +0700

    setup cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,9 @@ import { ProductModule } from './modules/product/product.moudule';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auht.module';
-
 @Module({
   imports: [ProductModule,UserModule,AuthModule,
-    MongooseModule.forRoot('mongodb://127.0.0.1/local'),],
+    MongooseModule.forRoot('mongodb+srv://trinhcongtrieu2972002:trieutct@trieu.q6n63sn.mongodb.net/'),],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe());
+
+
+  //setup cors
+  const whiteList = '*';
+  const corsOptions: CorsOptions = {
+    origin:
+      whiteList?.split(',')?.length > 1 ? whiteList.split(',') : whiteList,
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Language',
+      'X-Timezone',
+      'X-Timezone-Name',
+      'X-Mssp-Id',
+      'X-Organization-Id',
+    ],
+    optionsSuccessStatus: 200,
+    methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+  };
+  app.enableCors(corsOptions);
+
+
+  
   await app.listen(3000);
-  console.log("đang chạy cổng 3000")
+  console.log('đang chạy cổng 3000');
 }
 bootstrap();
