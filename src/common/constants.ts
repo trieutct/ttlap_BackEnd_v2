beffc3bdca26b25e1d650b13fe64142d28e0429e
trieutct <96654274+trieutct@users.noreply.github.com>
2024-02-15 23:16:52 +0700

    update expiresIn

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,6 +1,6 @@
 export const jwtConstants = {
     secret: 'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
-    expiresIn: 60,
+    expiresIn: '30s',
     refresh_expiresIn: '1d',
 };
 
